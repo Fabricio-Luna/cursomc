3a76a44400daeedad3b07bfc87ddac6cc64c35c9
Fabricio Luna <you@example.com>
2019-06-18 11:20:00 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/faber/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.faber.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.faber.domain.Categoria;
+import com.faber.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
